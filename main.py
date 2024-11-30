eed005e8d4c623303df3ff87281507ddc2680350
Marko Vuger <markovugerlol@gmail.com>
2024-11-30 03:04:55 +0100

    Feat:Added two lists

---
+++ main.py
@@ -0,0 +1,7 @@
+import random
+
+npcClass = ["Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard", "Artificer"]
+
+npcRace = ["Elf", "Dragonborn", "Dwarf", "Gnome", "Human", "Orc", "Tiefling", "Halfling"]
+
+
