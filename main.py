c5aee6408d592f4d8ca10a9ac97ca96219c71607
Marko Vuger <markovugerlol@gmail.com>
2024-11-30 03:26:41 +0100

    Feat: Added openAI API

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import random
-import openai
+from openai import OpenAI
+
+
+client = OpenAI(
+    api_key = "api key"
+)
 
 npcClass = ["Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard", "Artificer"]
 
@@ -9,9 +14,22 @@ def createNPC(raceList, classList):
     dndrace = random.choice(raceList)
     dndclass = random.choice(classList)
     
-    output = "Race: {} \nClass: {} \n".format(dndrace, dndclass)
+    prompt = f"Describe a {dndrace} npc who is a {dndclass} in Dungeons and Dragons, make a short, engaging description"
+    
+    response = client.completions.create(
+        model="davinci-002",
+        prompt=prompt,
+        max_tokens=150,
+        temperature=0.7
+    )
+    
+    description = response.choices[0].text.strip()
+    
+    output = "Race: {} \nClass: {} \nDescription: \n\n {}".format(dndrace, dndclass, description)
     
     return output
+
+
 
 
 print(createNPC(npcRace, npcClass))
