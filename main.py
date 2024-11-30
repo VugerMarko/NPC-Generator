8efff4e226433e04be2e8bd6ade3f38a45b04679
Marko Vuger <markovugerlol@gmail.com>
2024-11-30 03:09:01 +0100

    Feat: Added a function createNPC

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import random
+import openai
 
 npcClass = ["Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard", "Artificer"]
 
 npcRace = ["Elf", "Dragonborn", "Dwarf", "Gnome", "Human", "Orc", "Tiefling", "Halfling"]
 
+def createNPC(raceList, classList):
+    dndrace = random.choice(raceList)
+    dndclass = random.choice(classList)
+    
+    output = "Race: {} \nClass: {} \n".format(dndrace, dndclass)
+    
+    return output
+
+
+print(createNPC(npcRace, npcClass))
 
