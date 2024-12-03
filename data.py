dfd92e5d8d389acc0fdf84e72630ea4d49d2c043
Marko Vuger <markovugerlol@gmail.com>
2024-12-03 16:39:54 +0100

    Added: database functions to pull the data out of it

--- data.py
+++ data.py
@@ -1,31 +1,37 @@
-npcClass = ["Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard", "Artificer"]
+npcClass = [
+    "Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", 
+    "Paladin", "Ranger", "Rogue", "Sorcerer", 
+    "Warlock", "Wizard", "Artificer"
+]
 
 npcRace = ["Elf", "Dragonborn", "Dwarf", "Gnome", "Human", "Orc", "Tiefling", "Halfling"]
 
-Ability = {"STR": 0, 
-           "DEX": 0, 
-           "CON": 0, 
-           "INT": 0, 
-           "WIS": 0, 
-           "CHA": 0
+Ability = {
+    "STR": 0, 
+    "DEX": 0, 
+    "CON": 0, 
+    "INT": 0, 
+    "WIS": 0, 
+    "CHA": 0
 }
 
-skills = {"Acrobatics": "DEX", 
-          "Animal Handling": "WIS", 
-          "Arcana": "INT", 
-          "Athletics": "STR",
-          "Deception": "CHA",
-          "History": "INT",
-          "Insight": "WIS",
-          "Intimidation": "CHA",
-          "Investigation": "INT",
-          "Medicine": "WIS",
-          "Nature": "INT",
-          "Perception": "WIS",
-          "Performance": "CHA",
-          "Persuasion": "CHA",
-          "Religion": "INT",
-          "Sleight of Hand": "DEX",
-          "Stealth": "DEX",
-          "Survival": "WIS"
+skills = {
+    "Acrobatics": "DEX", 
+    "Animal Handling": "WIS", 
+    "Arcana": "INT", 
+    "Athletics": "STR",
+    "Deception": "CHA",
+    "History": "INT",
+    "Insight": "WIS",
+    "Intimidation": "CHA",
+    "Investigation": "INT",
+    "Medicine": "WIS",
+    "Nature": "INT",
+    "Perception": "WIS",
+    "Performance": "CHA",
+    "Persuasion": "CHA",
+    "Religion": "INT",
+    "Sleight of Hand": "DEX",
+    "Stealth": "DEX",
+    "Survival": "WIS"
 }
--- database.py
+++ database.py
@@ -1,33 +1,74 @@
 import sqlite3
+class DnDClass:
+    
+    def __init__(self, id_num = -1, className="", classAbility="", spellcaster = True):
+        self.id_num = id_num
+        self.className = className
+        self.classAbility = classAbility
+        self.spellcaster = spellcaster
+        self.connection = sqlite3.connect("Specifications.db")
+        self.cursor = self.connection.cursor()
+        
+    def loadClassName(self, className):
+        self.cursor.execute("""
+            SELECT * FROM classes
+            WHERE class_name = ?
+        """, (className, ))
+        
+        results = self.cursor.fetchone()
+        if results:
+            self.id_num = results[0]
+            self.className = results[1]
+            self.classAbility = results[2]
+            self.spellcaster = results[3]
+        else:
+            raise ValueError(f"Class with name {className} not found.")
+        
+        
+#Testing the database usage
+class1 = DnDClass()
 
-connection = sqlite3.connect("Specifications.db")
+try:
+    class1.loadClassName('Wizard')
+    print(f"ID: {class1.id_num}")
+    print(f"Class Name: {class1.className}")
+    print(f"Class Ability: {class1.classAbility}")
+    print(f"Is Spellcaster: {class1.spellcaster}")
+except ValueError as e:
+    print(e)
 
-cursor = connection.cursor()
 
-cursor.execute("""
-    CREATE TABLE IF NOT EXISTS classes(
-        class_name TEXT,
-        class_ability TEXT,
-        is_spellcaster BOOLEAN
-    )               
-""")
+#Old code for defining the database, want to keep it in case something happens
 
-cursor.execute("""
-    INSERT INTO classes VALUES
-        ('Cleric', 'WIS', TRUE),
-        ('Paladin', 'STR', TRUE),
-        ('Barbarian', 'STR', FALSE),
-        ('Bard', 'CHA', TRUE),
-        ('Druid', 'WIS', TRUE),
-        ('Fighter', 'DEX', FALSE),
-        ('Monk', 'DEX', FALSE),
-        ('Ranger', 'DEX', TRUE),
-        ('Rogue', 'DEX', FALSE),
-        ('Sorcerer', 'CHA', TRUE),
-        ('Warlock', 'CHA', TRUE),
-        ('Wizard', 'INT', TRUE)
-""")
+# connection = sqlite3.connect("Specifications.db")
 
-connection.commit()
+# cursor = connection.cursor()
 
-connection.close()
+# cursor.execute("""
+#     CREATE TABLE IF NOT EXISTS classes(
+#         id INTEGER PRIMARY KEY,
+#         class_name TEXT,
+#         class_ability TEXT,
+#         is_spellcaster BOOLEAN
+#     )               
+# """)
+
+# cursor.execute("""
+#     INSERT INTO classes VALUES
+#         (1, 'Cleric', 'WIS', TRUE),
+#         (2, 'Paladin', 'STR', TRUE),
+#         (3, 'Barbarian', 'STR', FALSE),
+#         (4, 'Bard', 'CHA', TRUE),
+#         (5, 'Druid', 'WIS', TRUE),
+#         (6, 'Fighter', 'DEX', FALSE),
+#         (7, 'Monk', 'DEX', FALSE),
+#         (8, 'Ranger', 'DEX', TRUE),
+#         (9, 'Rogue', 'DEX', FALSE),
+#         (10, 'Sorcerer', 'CHA', TRUE),
+#         (11, 'Warlock', 'CHA', TRUE),
+#         (12, 'Wizard', 'INT', TRUE)
+# """)
+
+# connection.commit()
+
+# connection.close()
